1833d2d550fd08591978838195974219688013b6
panshangzhu <yipan.wang677090@gmail.com>
2020-05-12 20:24:19 -0400

    Updates

---
+++ precache-manifest.834f0c774ac753312c82f46b3139d9fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6576a61b9ab39271ed94d669eccfc4bf",
+    "url": "/panCalculator/index.html"
+  },
+  {
+    "revision": "6914147e1178ece35b07",
+    "url": "/panCalculator/static/css/main.04518a57.chunk.css"
+  },
+  {
+    "revision": "c17482a86f1d6b4112cb",
+    "url": "/panCalculator/static/js/2.e7ec0075.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/panCalculator/static/js/2.e7ec0075.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6914147e1178ece35b07",
+    "url": "/panCalculator/static/js/main.377bccb4.chunk.js"
+  },
+  {
+    "revision": "cf2b69fece234b9241bc",
+    "url": "/panCalculator/static/js/runtime-main.4d781113.js"
+  }
+]);
